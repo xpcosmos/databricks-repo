bd986653146836901972f5f47454039d4fe5398e
mikeias.oliveira@al.infnet.edu.br <mikeias.oliveira@al.infnet.edu.br>
2026-08-29 16:32:50 +0000

    bronze: development of first ingestion & enhance on landing data

--- etl/01_cp_file.py
+++ etl/01_cp_file.py
@@ -1,12 +1,16 @@
+import sys, os
+from dbruntime.remotefshandler.RemoteFsHandler import *
+sys.path.append(os.path.abspath('./src'))
+
 from src import *
 
 SOURCE_PATH = (
     "file:/Workspace/Users/"
     "mikeias.oliveira@al.infnet.edu.br/"
-    "databricks-repo/etl/microdados_enem_2025"
+    "databricks-repo/etl/tmp/microdados_enem_2024"
 )
 
-TARGET_PATH = "dbfs:/Volumes/databricks-repo/enem/enem2025"
+TARGET_PATH = "dbfs:/Volumes/databricks-repo/bronze/raw_enem"
 
 datafile = Data(SOURCE_PATH, TARGET_PATH)
 datafile.cp()
--- pl_etl/transformations/00_bronze.py
+++ pl_etl/transformations/00_bronze.py
@@ -1,21 +1,38 @@
 from pyspark import pipelines as dp
 
+
+CATALOG_NAME = 'databricks-repo'
+SCHEMA_NAME = 'bronze'
+VOLUME_NAME = 'raw_enem'
+
+data_base_path = f"dbfs:/Volumes/{CATALOG_NAME}/{SCHEMA_NAME}/{VOLUME_NAME}/DADOS"
+
 @dp.table(name="itens_prova")
-def itens_prova():
+def tb_itens_prova():
     return (
         spark.read.format("csv")
         .option("header", "true")
         .option("sep", ";")
         .option("encoding", "iso-8859-1") 
-        .load("dbfs:/Volumes/databricks-repo/enem/enem2025/DADOS/ITENS_PROVA_2025.csv")
+        .load(data_base_path + "/ITENS_PROVA_2025.csv")
     )
 
 @dp.table(name="participantes")
-def participantes():
+def tb_participantes():
     return (
         spark.read.format("csv")
         .option("header", "true")
         .option("sep", ";")
         .option("encoding", "iso-8859-1")
-        .load("dbfs:/Volumes/databricks-repo/enem/enem2025/DADOS/PARTICIPANTES_2025.csv")
+        .load(data_base_path + "/PARTICIPANTES_2025.csv")
+    )
+
+@dp.table(name="resultados")
+def tb_resultados():
+    return (
+        spark.read.format("csv")
+        .option("header", "true")
+        .option("sep", ";")
+        .option("encoding", "iso-8859-1")
+        .load( data_base_path + "/RESULTADOS_2024.csv")
     )
