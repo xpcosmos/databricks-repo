13d351e6c7917f7b48cde43e62f979c51de474f9
mikeias.oliveira@al.infnet.edu.br <mikeias.oliveira@al.infnet.edu.br>
2026-08-31 22:26:57 +0000

    Add reorganized source files and bronze transformations
    - Added ingestion helper classes and utilities to etl/ingestion/src/
    - Added bronze layer transformations in etl/bronze/transformations/
    - Included utility notebook for schema operations

---
+++ etl/ingestion/00_download_file.py
@@ -0,0 +1,28 @@
+import io
+import zipfile
+import requests
+import os
+
+# # Set the URL and the target directory
+URL = 'https://download.inep.gov.br/microdados/microdados_enem_2024.zip'
+
+EXTRACT_TO_DIR = (
+    "/Workspace/Users/"
+    "mikeias.oliveira@al.infnet.edu.br/"
+    "databricks-repo/etl/tmp"
+)
+
+# Download the file
+response = requests.get(URL, verify=False, stream=True)
+
+with open("./microdados_enem_2024", "wb") as f:
+    for chunk in response.iter_content(chunk_size=1024 * 1024):
+        if chunk:
+            f.write(chunk)
+
+
+print(os.getcwd())
+# Read the bytes into memory and extract everything
+with open("./microdados_enem_2024", "rb") as f:
+    with zipfile.ZipFile(f) as zip_file:
+        zip_file.extractall(EXTRACT_TO_DIR)
---
+++ etl/ingestion/src/__init__.py
@@ -0,0 +1,3 @@
+from src.data import Data
+
+__all__ = ['Data']
